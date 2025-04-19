635eb3c1162a16e3ef7eedf9af38adf98d1f1736
Смирнов <maxah66@gmail.com>
2025-04-19 23:47:10 +0300

    wip

--- DoomExCommand.cs
+++ DoomExCommand.cs
@@ -6,7 +6,6 @@ using RevitDoom.Views;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace RevitDoom
@@ -15,19 +14,15 @@ namespace RevitDoom
     public class DoomExCommand : IExternalCommand
     {
         public static FlatFaceServer Server;
-
         static public UIApplication UiApp;
 
-
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {          
-
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
 
             UiApp = commandData.Application;
             var uidoc = UiApp.ActiveUIDocument;
-            var doc = uidoc.Document;
-
-     
+            var doc = uidoc.Document;     
 
             var wadPath = UserSelect.GetWad();
 
@@ -39,21 +34,6 @@ namespace RevitDoom
             //Server = new FlatFaceServer(uidoc, 320, 200, 0.025);
 
             RevitServices.RegisterServer(Server, uidoc, new HashSet<Document>() { doc });
-
-            //var myFloor = doc.GetElement(new ElementId(264131));
-            //Reference faceRef = HostObjectUtils.GetSideFaces((HostObject)myFloor, ShellLayerType.Interior).First();
-            //Face face = ((HostObject)myFloor).get_Geometry(new Options()).OfType<Solid>()
-            //    .SelectMany(s => s.Faces.Cast<Face>()).FirstOrDefault();
-
-            //var builder = new AppBuilder();
-            //builder.SetIwad(wadPath)
-            //    .EnableHighResolution(false)
-            //    .WithArgs("-skill", "3")
-            //    .WithScale(1)
-            //    .WithDocument(doc)
-            //    .WithUIDocument(uidoc)
-            //    .WithReferenceObj(faceRef)
-            //    .WithFaceObj(face);
 
             var builder = new AppBuilder();
             builder.SetIwad(wadPath)
